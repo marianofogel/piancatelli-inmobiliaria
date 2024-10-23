a7277feb3016e6d9388b0e3ea5eb0e4614b2d9c8
matiasntoledo <matiasntoledo@gmail.com>
2024-10-23 14:52:35 -0300

    commit changes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ import {
     Container,
     Stack,
     Image,
-    NavLink,
+
 } from "react-bootstrap";
 import { GoDot } from "react-icons/go";
 import {
@@ -14,11 +14,11 @@ import {
     FaPhoneAlt,
     FaEnvelope,
     FaWhatsapp,
-    FaDotCircle
+
 
 } from "react-icons/fa"
 import "./Footer.css";
-
+import { useNavigate } from "react-router";
 
 
 const fecha = new Date(); //tomamos la fecha actual
@@ -26,6 +26,8 @@ const anioActual = fecha.getFullYear(); //se toma el año actual
 
 
 const Footer = () => {
+
+    const navigate = useNavigate();
     return (
         <>
             <footer id="contact-form">
@@ -34,33 +36,44 @@ const Footer = () => {
                         <Col className="py-3 sm=true">
                             <Stack>
                                 <div className="mx-auto">
-                                    <Image className="logo-footer"
-                                        src={process.env.PUBLIC_URL + "/img/Piancatelli.png"}
-                                        alt="Company Logo"
-                                        width={230}
-                                        height={100}
-                                    />
+
+                                    <div>
+                                        <Image className="logo-footer"
+                                            src={process.env.PUBLIC_URL + "/img/Piancatelli.png"}
+                                            alt="Company Logo"
+                                            width={230}
+                                            height={100}
+                                        />
+                                    </div>
                                     <h2 className="nombre-footer">Alejandra Baeta</h2>
 
-                                    <div className="footer-contact">
-                                        <FaMapMarkerAlt />
-                                        Ricchieri 1534 - Hurlingham
-                                    </div>
 
-                                    <div className="footer-contact">
-                                        <FaEnvelope />
-                                        tumail@gmail.com
-                                    </div>
+                                    <Col>
+                                        <div className="footer-contact">
+                                            <FaMapMarkerAlt />
+                                            Ricchieri 1534
+                                        </div>
 
-                                    <div className="footer-contact">
-                                        <FaPhoneAlt />
-                                        4665-2438
-                                    </div>
 
-                                    <div className="footer-contact">
-                                        <FaWhatsapp />
-                                        1132817839
-                                    </div>
+
+                                        <div className="footer-contact">
+                                            <FaEnvelope />
+                                            tumail@gmail.com
+                                        </div>
+
+
+
+                                        <div className="footer-contact">
+                                            <FaPhoneAlt />
+                                            4665-2438
+                                        </div>
+
+                                        <div className="footer-contact">
+                                            <FaWhatsapp />
+                                            1132817839
+                                        </div>
+
+                                    </Col>
 
                                     <div className="link-insta" >
                                         <Link className="link-seguinos">Seguinos en Instagram
@@ -78,6 +91,7 @@ const Footer = () => {
                                         />
                                     </div>
                                      */}
+
                                 </div>
 
                             </Stack>
@@ -87,21 +101,33 @@ const Footer = () => {
                             <Stack >
                                 <div className="mx-auto">
                                     <h2 className="footer-busqueda">Búsqueda Rápida</h2>
-                                    <Link className="footer-links" >Ventas en Barrio Privados</Link>
-                                    <Link className="footer-links">Ventas en Countries</Link>
-                                    <Link className="footer-links">Alquileres en Barrios Privados</Link>
-                                    <Link className="footer-links">Alquileres en Countries</Link>
-                                    <Link className="footer-links">Casas en zona</Link>
-                                    <Link className="footer-links">Lotes en zona</Link>
-                                    <Link className="footer-links">Casas en zona</Link>
-                                    <Link className="footer-links">Lotes en zona</Link>
+                                    <div className="columna-footer">
+                                        <Row className="links-row">
+
+                                            <Col>
+                                                <Link /*onclick con el set de filtros*/ className="footer-links" >Ventas en Barrio Privados</Link>
+                                                <Link className="footer-links">Ventas en Countries</Link>
+                                                <Link className="footer-links">Casas en zona</Link>
+                                                <Link className="footer-links">Lotes en zona</Link>
+
+                                            </Col>
+
+                                            <Col>
+                                                <Link className="footer-links">Alquileres en Barrios Privados</Link>
+                                                <Link className="footer-links">Alquileres en Countries</Link>
+                                                <Link className="footer-links">Casas en zona</Link>
+                                                <Link className="footer-links">Lotes en zona</Link>
+                                            </Col>
+
+                                        </Row>
+                                    </div>
                                 </div>
                             </Stack>
                         </Col>
 
 
                     </Row>
-                </Container>
+                </Container >
             </footer >
 
             <footer>
