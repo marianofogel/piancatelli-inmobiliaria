0207ccb1d89583fddea4f2251cea4b3690586e46
Mariano <Marianofogel17@gmail.com>
2024-10-23 16:48:07 -0300

    Condiciones y Privacidad

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -118,8 +118,8 @@ const Footer = () => {
                     <Row className="footer-bottom">
                         <div className="footer-rights">
                             © {anioActual}  Todos los derechos reservados.
-                            <a href="" className="links-legales"> - Terminos y condiciones </a>
-                            <a href="" className="links-legales"> - Politica de Privacidad </a>
+                            <a href="/condiciones" className="links-legales"> - Terminos y condiciones </a>
+                            <a href="/privacidad" className="links-legales"> - Politica de Privacidad </a>
                         </div>
 
                     </Row>
