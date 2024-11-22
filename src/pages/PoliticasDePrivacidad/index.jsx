c1402ceec97972c2e0804267180e65f2815de1bf
Mariano <Marianofogel17@gmail.com>
2024-11-22 15:01:01 -0300

    Nuevos Cambios

--- src/pages/PoliticasDePrivacidad/index.jsx
+++ src/pages/PoliticasDePrivacidad/index.jsx
@@ -6,75 +6,70 @@ export default function PoliticasDePrivacidad() {
         <Container fluid className="mt-5 py-5">
             <Row className="container-contacto">
                 <Col className="col-contacto">
-                    
-                        <h2 className="pb-3">Politicas de Privacidad</h2>
-                        <div className="col-contacto">
-                            <p className="pt-5 m-1 text-align-center">
-                                Al navegar y utilizar el sitio{" "}
-                                <span style={{ fontWeight: "bold" }}>
-                                    www.piancatelli-propiedades.com.ar
-                                </span>{" "}
-                                el usuario declara haber leído y aceptado los términos y
-                                condiciones detallados aquí. Si el usuario no está de acuerdo
-                                con los términos y condiciones, debe salir inmediatamente del
-                                sitio. La Universidad Escuela Argentina de Negocios (en
-                                adelante, UEAN) se reserva el derecho de cambiar los términos y
-                                condiciones de uso de su sitio web en cualquier momento y sin
-                                previo aviso. Uso del contenido Los usuarios utilizarán el sitio{" "}
-                                <span style={{ fontWeight: "bold" }}>
-                                    www.piancatelli-propiedades.com.ar
-                                </span>{" "}
-                                exclusivamente para su uso personal. Queda expresamente
-                                prohibida la reproducción total o parcial de los contenidos
-                                protegidos por la ley de propiedad intelectual, así como el uso
-                                comercial de los contenidos del sitio, su venta o distribución a
-                                terceros, o su modificación. Cualquier otro uso requiere
-                                autorización previa escrita y explícita de la UEAN. Logos y
-                                Marcas Todos los logos y marcas de la UEAN exhibidos en el sitio{" "}
-                                <span style={{ fontWeight: "bold" }}>
-                                    www.piancatelli-propiedades.com.ar
-                                </span>{" "}
-                                están registrados y protegidos por la ley. Queda expresamente
-                                prohibida su reproducción sin autorización previa escrita y
-                                explícita de la UEAN. Cualquier otro logos y/o marca utilizados
-                                en el sitio{" "}
-                                <span style={{ fontWeight: "bold" }}>
-                                    www.piancatelli-propiedades.com.ar
-                                </span>{" "}
-                                son de propiedad de sus respectivos dueños. Veracidad del
-                                contenido La UEAN no garantiza la exactitud del contenido
-                                expresado en el sitio{" "}
-                                <span style={{ fontWeight: "bold" }}>
-                                    www.piancatelli-propiedades.com.ar
-                                </span>{" "}
-                                , pudiendo este tener errores u omisiones con respecto a la
-                                descripción de productos y servicios ofrecidos, incluyendo pero
-                                no limitándose a cursos, programas, carreras, profesores u
-                                otros. La UEAN se reserva el derecho de corregir estos errores
-                                sin previo aviso, y de cancelar cualquier programa, carrera o
-                                curso ofrecido sin previo aviso. Opinión Las opiniones
-                                expresadas en este sitio web son exclusivas de sus autores
-                                específicos, y no representan necesariamente la opinión, consejo
-                                o información de la UEAN. Enlaces a otros sitios web De existir
-                                en el sitio web{" "}
-                                <span style={{ fontWeight: "bold" }}>
-                                    www.piancatelli-propiedades.com.ar
-                                </span>{" "}
-                                enlaces hacia otros sitios web o empresas. El usuario acepta que
-                                la UEAN no acepta ninguna responsabilidad sobre el uso de otros
-                                sitios web, y que el mismo no recomienda particularmente ningún
-                                servicio o producto de terceros. Otras Empresas La UEAN no asume
-                                ninguna responsabilidad por los productos y servicios de
-                                terceros ofrecidos a través de su sitio web, ni por las acciones
-                                de ninguna persona, física o jurídica, especificadas en su sitio
-                                web. Legislación El uso del sitio{" "}
-                                <span style={{ fontWeight: "bold" }}>
-                                    www.piancatelli-propiedades.com.ar
-                                </span>{" "}
-                                y sus términos y condiciones se rigen por la ley argentina.
+                    <h2 className="pb-3" style={{ color: "red" }}>Politicas de Privacidad</h2>
+                    <div className="col-contacto">
+                        <p className="pt-5 m-1 text-align-center">
+                            La Política de Privacidad que se enuncia a continuación se aplica
+                            a toda información personal que los usuarios brinden durante la
+                            utilización del sitio www.piancatelli-propiedades.edu.ar.
+                            <p style={{
+                                margin: "15px"
+                            }}>
+                                <span style={{ fontWeight: "bold" }}>1- Destino de la información recopilada:</span> Los datos personales
+                                que los usuarios brinden, serán utilizados para proveerle al
+                                usuario información sobre ventas y alquileres, así como otros
+                                intereses relacionados. Piancatelli Propiedades expresa que los
+                                datos personales objeto de tratamiento no serán utilizados para
+                                finalidades distintas o incompatibles con las que motivaron su
+                                obtención, y serán utilizados únicamente por este.
                             </p>
-                        </div>
-                    
+                            <p style={{
+                                margin: "15px"
+                            }}>
+                                <span style={{ fontWeight: "bold" }}>2- Aceptación de los términos y condiciones:</span> Al brindar sus datos
+                                personales, los usuarios autorizan a Piancatelli Propiedades a
+                                comunicarse con ellos por vía telefónica o electrónica enviando
+                                información que pueda ser de su interés, incluyendo publicidad e
+                                información sobre temas inmobiliarios.
+                            </p>
+                            <p style={{
+                                margin: "15px"
+                            }}>
+                                <span style={{ fontWeight: "bold" }}>3- Base de Datos:</span> Los datos
+                                recabados por los formularios correspondientes serán incorporados
+                                a una base de datos de Piancatelli Propiedades, sita en Dirección
+                                DISTRITO T - Oficina 118 - Colectora Norte Acceso Oeste km 47 -
+                                General Rodriguez - Buenos Aires, Argentina. Los usuarios tendrán
+                                derecho a solicitar el retiro o bloqueo de su nombre de los bancos
+                                de datos en cualquier momento. En caso de que los usuarios no
+                                deseen ser contactados con estos fines, podrán notificarlo
+                                fehacientemente a info@piancatelli-propiedades.com.ar , y se
+                                procederá a interrumpir este tipo de comunicaciones en el menor
+                                tiempo que le sea posible.
+                            </p>
+                            <p style={{
+                                margin: "15px"
+                            }}>
+                                <span style={{ fontWeight: "bold" }}>4- Seguridad de protección de los
+                                datos:</span> Piancatelli Propiedades realiza su mejor esfuerzo para la
+                                protección de los datos personales legalmente requeridos y procura
+                                instalar medios y medidas técnicas de protección. No obstante, el
+                                usuario debe ser consciente de que las medidas de seguridad en
+                                Internet no son infranqueables.
+                            </p>
+                            <p style={{
+                                margin: "15px"
+                            }}>
+                                <span style={{ fontWeight: "bold" }}>5- Exclusión de garantías y de
+                                responsabilidad:</span> Piancatelli Propiedades excluye, con toda la
+                                extensión permitida por el ordenamiento jurídico, cualquier
+                                responsabilidad por los daños y perjuicios de toda naturaleza que
+                                puedan llegar a ocurrir. En todo aspecto, resultará aplicable los
+                                términos y condiciones que regula el uso del servicio del sitio
+                                www.piancatelli-propiedades.com.ar que Piancatelli Propiedades
+                                pone a disposición de los usuarios de Internet. </p>
+                        </p>
+                    </div>
                 </Col>
             </Row>
         </Container>
