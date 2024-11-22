4d79614c343c4a296c69b0867849a72f39185ba4
Mariano <Marianofogel17@gmail.com>
2024-11-22 11:39:33 -0300

    Pruebas

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,8 +5,6 @@ import { Buscador } from "./Buscador/Buscador";
 import { NuevosIngresos } from "./NuevosIngresos/NuevosIngresos";
 import { Element } from "react-scroll";
 import React from "react";
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
 
 export default function Home() {
   return (
