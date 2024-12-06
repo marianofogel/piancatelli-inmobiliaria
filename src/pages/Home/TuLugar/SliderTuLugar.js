95aa7540771caef97657a5fa41ae433525398e16
Mariano <Marianofogel17@gmail.com>
2024-12-06 16:02:25 -0300

    A ver si anda en notebook
    Subido a mi rama, a ver como anda desde la notebook

--- src/pages/Home/TuLugar/SliderTuLugar.js
+++ src/pages/Home/TuLugar/SliderTuLugar.js
@@ -9,8 +9,66 @@ import "swiper/css/navigation";
 import "./slider.css";
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
 import { barrios } from "../../../utils";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import useFilterStore from "../../../store";
+
 
 const SliderTuLugar = () => {
+  const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
+  const barrio = barrios.find((item) => item.location_id === +id);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      setError(null);
+      try {
+        const queryParams = new URLSearchParams({
+          format: "json",
+          key: process.env.REACT_APP_TOKKO_API_KEY,
+          lang: "es_ar",
+          limit: 4,
+          data: JSON.stringify({
+            operation_types: [1, 2, 3],
+            property_types: [1, 2, 3, 4, 5, 6, 7],
+            price_from: 0,
+            price_to: 99999999,
+            current_localization_id: [+id],
+            current_localization_type: "division",
+          }),
+        });
+
+        const response = await fetch(
+          `http://tokkobroker.com/api/v1/property/search?${queryParams.toString()}`
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const result = await response.json();
+        setData(result.objects);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const { setFilters } = useFilterStore();
+  const navigate = useNavigate();
+
+  const handleCardClick = (locationId) => {
+    setFilters({ localizationId: locationId  });
+    // Lo que le pasemos en localidad va a ser el filtro que va a aparecer despues del navigate
+    navigate("/propiedades");
+  };
+
+
+
   return (
     <div className="swiper-contenedor-tulugar container-fluid">
       <Swiper
@@ -49,7 +107,8 @@ const SliderTuLugar = () => {
               <TuLugarCard
                 imageSrc={process.env.PUBLIC_URL + "/img/barrios/" + barrio.location_id + ".webp"}
                 barrio={barrio.location_name}
-                value={barrio.location_id}
+                data={data} // Pasamos los datos a la card
+                onButtonClick={() => handleCardClick(barrio.location_id)} // Pasamos la función
               />
             </Link>
           </SwiperSlide>
--- src/pages/Home/TuLugar/TuLugarCard.js
+++ src/pages/Home/TuLugar/TuLugarCard.js
@@ -1,18 +1,31 @@
-import { Image } from "react-bootstrap";
+import { Image, Button } from "react-bootstrap";
 
-const TuLugarCard = ({ imageSrc, barrio, onClick }) => {
+
+const TuLugarCard = ({ imageSrc, barrio, onClick, data, onButtonClick }) => {
 
     return (
-        <div className='card-tulugar' onClick={onClick}>
-            <Image className='imagen-tulugar'
-                src={imageSrc}
-            />
-            <p className='texto-tulugar'>{barrio}</p>
-        </div>
+        <>
+            <div className='card-tulugar' onClick={onClick}>
+                <Image className='imagen-tulugar'
+                    src={imageSrc}
+                />
+                <p className='texto-tulugar'>{barrio}</p>
+
+                {data && data.length ? (
+                    <Button
+                        variant="danger"
+                        className="mt-2 mb-3 boton-filtros-tulugar"
+                        onClick={ 
+                            onButtonClick
+                        }
+                    >
+                        Ver más propiedades
+                    </Button>
+                ) : " "}
+            </div>
+        </>
+
     )
-
-
-
 }
 
 export { TuLugarCard }
